af9ca42a01779f223572de76e407252ff0ff5215
May-mqy <mayar_234@outlook.com>
2025-11-23 22:06:47 -0300

    Todas as Tabelas/Entidades

--- src/main/java/com/librali/model/PessoaFisica.java
+++ src/main/java/com/librali/model/PessoaFisica.java
@@ -1,21 +1,28 @@
 package com.librali.model;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "pessoa_fisica")
 public class PessoaFisica {
 
     @Id
+    @Column (name = "pk_id_pf")
     private Integer pkIdPf;
 
     private String pronome;
+
+    @Column (name = "sobre_pf")
     private String sobrePf;
 
     @OneToOne
     @MapsId
     @JoinColumn(name = "pk_id_pf")
     private Usuario usuario;
-
-    // Getters e Setters
 }
